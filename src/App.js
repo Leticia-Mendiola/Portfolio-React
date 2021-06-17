e00ef2cb53ada01b9dd993917f2594046ce066a9
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-17 17:02:08 -0500

    added imports to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Resume from "./pages/Resume";
+import Profiles from "./pages/Profiles"
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Project from "./components/Project"
+// import Wrapper from "./components/Wrapper";
 import './App.css';
 
 function App() {
