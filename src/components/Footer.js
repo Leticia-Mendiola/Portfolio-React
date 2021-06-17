db16cafeb7e23174c8ab5ba4a257adc85f8b5fd0
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-17 16:52:49 -0500

    changed all class to className

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,18 +4,18 @@ import React from "react";
 function Footer() {
   return (
     <footer className="footer">
-        <div class="container">
-            <div class="row">
-                <div class="col l6 s12">
-                    <h5 class="white-text">Portfolio of Leticia Mendiola</h5>
-                    <p class="grey-text text-lighten-4">Leticia Mendiola is a full time student enrolled in the University of Texas at San Antonio (UTSA) Full Stack Web Development Bootcamp. Albeit skilled in full stack programing, her passion and strenghts are notably in front-end web develpment and design. She looks forward to launching her exciting new career following graduation in July 2021.</p>
+        <div className="container">
+            <div className="row">
+                <div className="col l6 s12">
+                    <h5 className="white-text">Portfolio of Leticia Mendiola</h5>
+                    <p className="grey-text text-lighten-4">Leticia Mendiola is a full time student enrolled in the University of Texas at San Antonio (UTSA) Full Stack Web Development Bootcamp. Albeit skilled in full stack programing, her passion and strenghts are notably in front-end web develpment and design. She looks forward to launching her exciting new career following graduation in July 2021.</p>
                 </div>
             </div>
         </div>
-        <div class="footer-copyright">
-            <div class="container">
+        <div className="footer-copyright">
+            <div className="container">
                 <span>Leticia Mendiola { new Date().getFullYear() }</span>
-                <a class="grey-text text-lighten-4 right" href="mailto: lmendiola509@gmail.com">Email</a>
+                <a className="grey-text text-lighten-4 right" href="mailto: lmendiola509@gmail.com">Email</a>
             </div>
         </div>
     </footer>
