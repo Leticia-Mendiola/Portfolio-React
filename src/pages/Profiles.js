8e8f451f48ff80f34193f71deba3e1e42c13040c
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-18 16:00:40 -0500

    created parent <div> for profiles page

--- src/pages/Profiles.js
+++ src/pages/Profiles.js
@@ -2,7 +2,9 @@ import React from "react";
 
 function Profiles() {
     return (
+    <div>
         <a href="https://github.com/Leticia-Mendiola">https://github.com/Leticia-Mendiola</a>
+    </div>
     );
 }
 
