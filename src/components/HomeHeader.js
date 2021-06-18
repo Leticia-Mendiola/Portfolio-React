b4fdd4d5edd85de4ac1b88644d5b00426e7c927a
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-18 16:33:43 -0500

    removed JSX from Home.js to its own 'HomeHeader' component

---
+++ src/components/HomeHeader.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function HomeHeader() {
+    return (
+        <div style="margin-top: 70px;" class="container row z-depth-5">
+        <img class="col s4 responsive-img circle" alt="profile pic" src="Assets/profile pic.jpg"/> 
+            <div class="col s8 card-content">
+                <h4 class="right-align">Contact Information</h4>
+                <br></br>
+                <h6 class="right-align">Phone:</h6>
+                <br></br>
+                <h6 class="right-align">210-608-5800</h6>
+                <br></br>
+                <h6 class="right-align">Email:</h6>
+                <h6 class="right-align">lmendiola509@gmail.com</h6>
+            </div>
+    </div>
+    );
+}
+
+export default HomeHeader
