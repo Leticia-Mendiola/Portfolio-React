0c6b2fb3207de7791a8b3fd8140d407eeef35368
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-19 21:07:34 -0500

    added second home route

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ function App() {
       <Header />
       <Wrapper>
         <Route exact path="/" component={Home} />
+        <Route exact path="/Home" component={Home} />
         <Route exact path="/Resume" component={Resume} />
         <Route exact path="/Profiles" component={Profiles} />
       </Wrapper>
