187fd2757075cae1f41d02c04224981e14f54b23
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-19 22:14:59 -0500

    created projects.json, props, added wrapper and map()

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,22 @@
 import React from "react";
 import HomeHeader from "../components/HomeHeader";
+import Wrapper from "../components/Wrapper";
 import Project from "../components/Project";
+import projects from "../projects.json";
 
 function Home() {
     return (
+        <Wrapper>
         <HomeHeader />,
-        <Project />
+        { projects.map(projects => (
+            <Project
+            name={projects.name}
+            image={projects.image}
+            occupation={projects.dlink}
+            location={projects.glink}
+            />))
+          }
+        </Wrapper>
     );
 }
 
