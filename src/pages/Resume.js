106e76414fcd08d10c0008d0899e3502a5b282c5
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-21 13:05:23 -0500

    adjusted formatting

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -8,7 +8,7 @@ function Resume() {
         <iframe title="Leticia Mendiola's Resume" style={{width:"100%", height:"1000px", border:"2px solid"}} src={"https://docs.google.com/document/d/e/2PACX-1vTSHICmYJlKDhgmdi06I16v_hksIzIJNUITC8LrtxSLfeVPPD8UR_4JY9KKI-KkhHBx_IuiWg49M5aQ/pub?embedded=true"}></iframe>
         </div>
         <div className="row align-items-center" style={{marginTop:"20px"}}>
-        <a href={pdf} download className="waves-effect waves-light btn-large" style={{marginBottom:"25px"}}>Download Resume</a>
+        <a href={pdf} download className="btn-large" style={{marginBottom:"25px"}}>Download Resume</a>
         </div>
     </div>
     );
