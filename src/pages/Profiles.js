cf8d093124d310193772ba643f905469a4ce96a4
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-21 14:06:54 -0500

    finalized profiles page design

--- src/pages/Profiles.js
+++ src/pages/Profiles.js
@@ -6,24 +6,29 @@ function Profiles() {
     <div>
         <div style={{marginTop: "70px"}} className="container row z-depth-5"v>
             <img className="col-4 img-fluid rounded-circle" alt="profile pic" src={pic}/> 
-            <div className="col-8 card-content valign-wrapper center-align">
+            <div className="col-8 card-content text-center align-middle">
+            <br></br>
             <div>
                 <h4 className="white-text">Git Hub Profile</h4>
             </div>
+            <br></br>
             <div>
                 <a href="https://github.com/Leticia-Mendiola">https://github.com/Leticia-Mendiola</a>
+            </div>
+            <br></br>
+            <div>
+                <h4 className="white-text">LinkedIn Profile</h4>
+            </div>
+            <br></br>
+            <div>
+                <a href="https://www.linkedin.com/in/leticia-mendiola-598a7183/">https://www.linkedin.com/in/leticia-mendiola-598a7183/</a>
             </div>
             </div>
         </div>
         <div style={{marginTop: "70px"}} className="container row z-depth-5">
-            <img className="col-4 img-fluid rounded-circle" alt="profile pic" src={pic}/> 
-            <div className="col-8 card-content valign-wrapper center-align">
-            <div>
-                <h4 className="white-text">LinkedIn Profile</h4>
-            </div>
-            <div>
-                <a href="https://www.linkedin.com/in/leticia-mendiola-598a7183/">https://www.linkedin.com/in/leticia-mendiola-598a7183/</a>
-            </div>
+            {/* <img className="col-4 img-fluid rounded-circle" alt="profile pic" src={pic}/>  */}
+            <div className="col-8 card-content text-center align-middle">
+            
             </div>
         </div>
     </div>
