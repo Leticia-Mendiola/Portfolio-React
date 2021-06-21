fc571f6724db99f6e2967c0cebb6710a44323d8d
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-21 13:20:19 -0500

    changed all class to className and fixed typo with anchor tag

--- src/pages/Profiles.js
+++ src/pages/Profiles.js
@@ -6,9 +6,9 @@ function Profiles() {
     <div>
         <div style={{marginTop: "70px"}} className="container row z-depth-5"v>
             <img className="col s4 responsive-img circle" alt="profile pic" src={pic}/> 
-            <div class="col s8 card-content valign-wrapper center-align">
+            <div className="col s8 card-content valign-wrapper center-align">
             <div>
-                <h4 class="white-text">Git Hub Profile</h2>
+                <h4 className="white-text">Git Hub Profile</h4>
             </div>
             <div>
                 <a href="https://github.com/Leticia-Mendiola">https://github.com/Leticia-Mendiola</a>
@@ -17,12 +17,12 @@ function Profiles() {
         </div>
         <div style={{marginTop: "70px"}} className="container row z-depth-5">
             <img className="col s4 responsive-img circle" alt="profile pic" src={pic}/> 
-            <div class="col s8 card-content valign-wrapper center-align">
+            <div className="col s8 card-content valign-wrapper center-align">
             <div>
-                <h4 class="white-text">LinkedIn Profile</h2>
+                <h4 className="white-text">LinkedIn Profile</h4>
             </div>
             <div>
-                <a href="https://www.linkedin.com/in/leticia-mendiola-598a7183/">https://www.linkedin.com/in/leticia-mendiola-598a7183/</h4>
+                <a href="https://www.linkedin.com/in/leticia-mendiola-598a7183/">https://www.linkedin.com/in/leticia-mendiola-598a7183/</a>
             </div>
             </div>
         </div>
