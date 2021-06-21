5185a69000184f0e80058d81f9978994f94c8c70
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-21 14:34:32 -0500

    changed styling of HomeHeader component

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -3,18 +3,21 @@ import pic from "../ProfilePic.jpg";
 
 function HomeHeader() {
     return (
-        <div style={{marginTop: "70px"}} className="container row z-depth-5">
-            <img className="col-4 img-fluid rounded-circle" alt="profile pic" src={pic}/> 
-            <div className="col-8 card-content">
-                <h4 className="right-align">Contact Information</h4>
+        <div style={{marginTop: "70px"}} className="container align-items-center row z-depth-5">
+            <div className="col-2"></div>
+            <img className="col-4 img-fluid rounded-circle" alt="profile pic" src={pic}/>
+            <div className="col-2"></div> 
+            <div className="col-3 card-content">
+                <h4 className="text-white">Contact Information</h4>
                 <br></br>
-                <h6 className="right-align">Phone:</h6>
+                <h6 className="text-white">Phone:</h6>
                 <br></br>
-                <h6 className="right-align">210-608-5800</h6>
+                <h6 className="text-white">210-608-5800</h6>
                 <br></br>
-                <h6 className="right-align">Email:</h6>
-                <h6 className="right-align">lmendiola509@gmail.com</h6>
+                <h6 className="text-white">Email:</h6>
+                <h6 className="text-white">lmendiola509@gmail.com</h6>
             </div>
+            <div className="col-1"></div>
         </div>
     );
 }
