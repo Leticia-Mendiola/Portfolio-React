1543abb712e360870862122b6f748d0e0d47e06f
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-21 12:33:24 -0500

    cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
@@ -6,29 +5,12 @@ import Resume from "./pages/Resume";
 import Profiles from "./pages/Profiles"
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-// import Project from "./components/Project"
 import Wrapper from "./components/Wrapper";
 import './App.css';
 
 function App() {
   return (
     <Router>
-    {/* <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div> */}
     <div>
       <Header />
       <Wrapper>
