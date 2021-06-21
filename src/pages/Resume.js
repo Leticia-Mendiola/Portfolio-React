f5171f456043f7063dd8f2b117618687ef6ac2b9
Leticia Mendiola <lmendiola509@gmail.com>
2021-06-21 11:19:26 -0500

    hefty reformatting of classes and inline styles

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,11 +3,11 @@ import React from "react";
 function Resume() {
     return (
     <div>
-        <div style="margin-top:20px; margin-right:5px; margin-left:5px;">
-        <iframe title="Leticia Mendiola's Resume" style="width:100%; height:1000px; border:2px solid;" src="https://docs.google.com/document/d/e/2PACX-1vTSHICmYJlKDhgmdi06I16v_hksIzIJNUITC8LrtxSLfeVPPD8UR_4JY9KKI-KkhHBx_IuiWg49M5aQ/pub?embedded=true"></iframe>
+        <div style={{marginTop: "20px", marginRight: "5px", marginLeft: "5px"}}>
+        <iframe title="Leticia Mendiola's Resume" style={{width:"100%", height:"1000px", border:"2px solid"}} src={"https://docs.google.com/document/d/e/2PACX-1vTSHICmYJlKDhgmdi06I16v_hksIzIJNUITC8LrtxSLfeVPPD8UR_4JY9KKI-KkhHBx_IuiWg49M5aQ/pub?embedded=true"}></iframe>
         </div>
-        <div class="center-align" style="margin-top:20px;">
-        <a href="Assets/Brand Statement.pdf" download class="waves-effect waves-light btn-large" style="margin-bottom:25px;">Download Resume</a>
+        <div className="center-align" style={{marginTop:"20px"}}>
+        <a href="Assets/Brand Statement.pdf" download class="waves-effect waves-light btn-large" style={{marginBottom:"25px"}}>Download Resume</a>
         </div>
     </div>
     );
